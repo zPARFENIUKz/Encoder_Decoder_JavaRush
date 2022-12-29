e1c4165c6841f3cbabe70b6d1ce441a0b5e77dc2
Gregory <parfeniuk33@gmail.com>
2022-12-29 20:49:20 +0300

    CryptoAnalyzerNullPointerException constructors created

--- src/CryptoAnalyzer/CryptoAnalyzerExceptions/CryptoAnalyzerNullPointerException.java
+++ src/CryptoAnalyzer/CryptoAnalyzerExceptions/CryptoAnalyzerNullPointerException.java
@@ -1,5 +1,18 @@
 package CryptoAnalyzer.CryptoAnalyzerExceptions;
 
 public class CryptoAnalyzerNullPointerException extends RuntimeException {
+    public CryptoAnalyzerNullPointerException() {
+    }
 
+    public CryptoAnalyzerNullPointerException(String message) {
+        super(message);
+    }
+
+    public CryptoAnalyzerNullPointerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CryptoAnalyzerNullPointerException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/CryptoAnalyzer/RussianCryptoAnalyzer/RussianCryptoAnalyzer.java
+++ src/CryptoAnalyzer/RussianCryptoAnalyzer/RussianCryptoAnalyzer.java
@@ -1,6 +1,7 @@
 package CryptoAnalyzer.RussianCryptoAnalyzer;
 
 import CryptoAnalyzer.CryptoAnalyzer;
+import CryptoAnalyzer.CryptoAnalyzerExceptions.CryptoAnalyzerNullPointerException;
 import CryptoAnalyzer.DecodeType.DecodeType;
 
 import java.nio.file.Path;
