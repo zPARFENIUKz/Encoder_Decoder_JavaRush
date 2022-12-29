560ad856e927a550a12f8cc0b7cf1c789054d464
Gregory <parfeniuk33@gmail.com>
2022-12-29 21:11:36 +0300

    CryptoAnalyzerIOException implemented

---
+++ src/CryptoAnalyzer/CryptoAnalyzerExceptions/CryptoAnalyzerIOException.java
@@ -0,0 +1,20 @@
+package CryptoAnalyzer.CryptoAnalyzerExceptions;
+
+import java.io.IOException;
+
+public class CryptoAnalyzerIOException extends RuntimeException {
+    public CryptoAnalyzerIOException() {
+    }
+
+    public CryptoAnalyzerIOException(String message) {
+        super(message);
+    }
+
+    public CryptoAnalyzerIOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CryptoAnalyzerIOException(Throwable cause) {
+        super(cause);
+    }
+}
