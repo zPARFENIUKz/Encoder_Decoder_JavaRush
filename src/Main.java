b39e6f76fa3bf4be9076d3c4edec4da2c5fc9f5d
Gregory <parfeniuk33@gmail.com>
2022-12-29 20:18:28 +0300

    getting Ru alphabet

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int firstLetterIndex = (int) 'А';
         int lastLetterIndex = (int) 'Я';
         System.out.println(firstLetterIndex);
         System.out.println(lastLetterIndex);
+
+        List<Character> alphabetRu = new ArrayList<>(lastLetterIndex - firstLetterIndex);
+        for (int i = firstLetterIndex; i <= lastLetterIndex; ++i) {
+            alphabetRu.add((char) i);
+        }
+        alphabetRu.forEach(System.out::print);
     }
 }
