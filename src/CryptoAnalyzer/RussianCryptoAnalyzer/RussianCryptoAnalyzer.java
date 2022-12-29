412834cf5d8f69d1159d54442d3fbcf0a6f3e67a
Gregory <parfeniuk33@gmail.com>
2022-12-29 20:41:21 +0300

    RussianCryptoAnalyzer initializing added

--- src/CryptoAnalyzer/RussianCryptoAnalyzer/RussianCryptoAnalyzer.java
+++ src/CryptoAnalyzer/RussianCryptoAnalyzer/RussianCryptoAnalyzer.java
@@ -4,8 +4,25 @@ import CryptoAnalyzer.CryptoAnalyzer;
 import CryptoAnalyzer.DecodeType.DecodeType;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RussianCryptoAnalyzer implements CryptoAnalyzer {
+    private static final String PUNCTUATION_CHARACTERS = " ,.?!-:;\"'";
+    private static final List<Character> ENCODED_CHARACTERS;
+    static {
+        final int firstLetterIndex = (int) 'А';
+        final int lastLetterIndex = (int) 'Я';
+        ENCODED_CHARACTERS = new ArrayList<>(lastLetterIndex - firstLetterIndex);
+        for (int i = firstLetterIndex; i <= lastLetterIndex; ++i) {
+            ENCODED_CHARACTERS.add((char) i);
+        }
+        for (final int c : PUNCTUATION_CHARACTERS.toCharArray()) {
+            ENCODED_CHARACTERS.add((char) c);
+        }
+        ENCODED_CHARACTERS.forEach(System.out::print);
+    }
+
     @Override
     public void encode(Path src, Path dest, int key) {
 
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,25 @@
+import CryptoAnalyzer.RussianCryptoAnalyzer.RussianCryptoAnalyzer;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int firstLetterIndex = (int) 'А';
-        int lastLetterIndex = (int) 'Я';
-        System.out.println(firstLetterIndex);
-        System.out.println(lastLetterIndex);
-
-        List<Character> alphabetRu = new ArrayList<>(lastLetterIndex - firstLetterIndex);
-        for (int i = firstLetterIndex; i <= lastLetterIndex; ++i) {
-            alphabetRu.add((char) i);
-        }
-        alphabetRu.forEach(System.out::print);
-        List<Character> allChars = new ArrayList<>(alphabetRu);
-        for (final int c : " ,.?!-:;\"'".toCharArray()) {
-            allChars.add((char) c);
-        }
-        allChars.forEach(System.out::print);
+//        int firstLetterIndex = (int) 'А';
+//        int lastLetterIndex = (int) 'Я';
+//        System.out.println(firstLetterIndex);
+//        System.out.println(lastLetterIndex);
+//
+//        List<Character> alphabetRu = new ArrayList<>(lastLetterIndex - firstLetterIndex);
+//        for (int i = firstLetterIndex; i <= lastLetterIndex; ++i) {
+//            alphabetRu.add((char) i);
+//        }
+//        alphabetRu.forEach(System.out::print);
+//        List<Character> allChars = new ArrayList<>(alphabetRu);
+//        for (final int c : " ,.?!-:;\"'".toCharArray()) {
+//            allChars.add((char) c);
+//        }
+//        allChars.forEach(System.out::print);
+        new RussianCryptoAnalyzer();
     }
 }
