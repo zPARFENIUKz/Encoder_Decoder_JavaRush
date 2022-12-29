fffd2f6834059946483883f6ef416ffb163567f2
Gregory <parfeniuk33@gmail.com>
2022-12-29 20:15:47 +0300

    first and last letter index

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstLetterIndex = (int) 'А';
+        int lastLetterIndex = (int) 'Я';
+        System.out.println(firstLetterIndex);
+        System.out.println(lastLetterIndex);
+    }
+}
