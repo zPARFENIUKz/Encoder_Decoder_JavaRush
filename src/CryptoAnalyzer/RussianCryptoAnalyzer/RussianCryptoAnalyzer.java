99aabb88313211c5367beceda97e8bf49fd9137a
Gregory <parfeniuk33@gmail.com>
2022-12-29 20:34:48 +0300

    RussianCryptoAnalyzer structure added

---
+++ src/CryptoAnalyzer/RussianCryptoAnalyzer/RussianCryptoAnalyzer.java
@@ -0,0 +1,23 @@
+package CryptoAnalyzer.RussianCryptoAnalyzer;
+
+import CryptoAnalyzer.CryptoAnalyzer;
+import CryptoAnalyzer.DecodeType.DecodeType;
+
+import java.nio.file.Path;
+
+public class RussianCryptoAnalyzer implements CryptoAnalyzer {
+    @Override
+    public void encode(Path src, Path dest, int key) {
+
+    }
+
+    @Override
+    public void decode(Path scr, Path dest, int key) {
+
+    }
+
+    @Override
+    public void decode(Path src, Path dest, DecodeType type) {
+
+    }
+}
