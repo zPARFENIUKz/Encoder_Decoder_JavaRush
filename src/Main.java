62a875907d0450416a3c0bd6603e8ddced2a2ef3
Gregory <parfeniuk33@gmail.com>
2022-12-29 21:35:43 +0300

    failed decode test

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,11 @@ import java.util.List;
 public class Main {
     private static final Path SRC_PATH = Path.of("src\\Files\\src.txt");
     private static final Path DEST_PATH = Path.of("src\\Files\\dest.txt");
+    private static final Path DECODED_PATH = Path.of("src\\Files\\decoded.txt");
+    private static final int KEY = 463;
     public static void main(String[] args) {
         CryptoAnalyzer analyzer = new RussianCryptoAnalyzer();
-        // key is 1 -> there must be all letters shifted by 1
-        //analyzer.encode(DEST_PATH, SRC_PATH, -1);
+        analyzer.encode(SRC_PATH, DEST_PATH, KEY);
+        analyzer.decode(DEST_PATH, DECODED_PATH, KEY);
     }
 }
