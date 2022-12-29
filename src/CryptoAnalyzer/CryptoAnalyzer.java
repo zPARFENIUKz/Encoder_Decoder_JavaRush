c280720ef16adcee2e4bcbededbf0f1112ccc13a
Gregory <parfeniuk33@gmail.com>
2022-12-29 20:29:49 +0300

    abstract logic added

---
+++ src/CryptoAnalyzer/CryptoAnalyzer.java
@@ -0,0 +1,11 @@
+package CryptoAnalyzer;
+
+import CryptoAnalyzer.DecodeType.DecodeType;
+
+import java.nio.file.Path;
+
+public interface CryptoAnalyzer {
+    void encode(Path src, Path dest, int key);
+    void decode(Path scr, Path dest, int key);
+    void decode(Path src, Path dest, DecodeType type);
+}
---
+++ src/CryptoAnalyzer/DecodeType/DecodeType.java
@@ -0,0 +1,6 @@
+package CryptoAnalyzer.DecodeType;
+
+public enum DecodeType {
+    BRUT_FORCE,
+    STATISTIC_ANALYSIS;
+}
